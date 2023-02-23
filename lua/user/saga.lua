933b595b5129d8bb50ca0373ed4d2f1ff08320aa
Hanan Asyrawi Rivai <hanan.rivai@bitcoin.co.id>
2023-02-23 14:26:34 +0700

    update saga go

--- lua/user/saga.lua
+++ lua/user/saga.lua
@@ -3,30 +3,45 @@ local saga = require('lspsaga')
 
 
 saga.setup({
-  show_outline = {
-    win_position = 'right',
-    --set special filetype win that outline window split.like NvimTree neotree
-    -- defx, db_ui
-    win_with = '',
-    win_width = 100,
-    auto_enter = true,
-    auto_preview = true,
-    virt_text = '┃',
-    jump_key = 'o',
-    -- auto refresh when change buffer
-    auto_refresh = true,
-  },
-  symbol_in_winbar = {
-    in_custom = false,
-    separator = ' ',
-    show_file = true,
-    -- define how to customize filename, eg: %:., %
-    -- if not set, use default value `%:t`
-    -- more information see `vim.fn.expand` or `expand`
-    -- ## only valid after set `show_file = true`
-    file_formatter = "",
-    click_support = true,
-  },
+    show_outline = {
+        win_position = 'right',
+        --set special filetype win that outline window split.like NvimTree neotree
+        -- defx, db_ui
+        win_with = '',
+        win_width = 100,
+        auto_enter = true,
+        auto_preview = true,
+        virt_text = '┃',
+        jump_key = 'o',
+        -- auto refresh when change buffer
+        auto_refresh = true,
+    },
+    symbol_in_winbar = {
+        in_custom = false,
+        separator = ' ',
+        show_file = true,
+        -- define how to customize filename, eg: %:., %
+        -- if not set, use default value `%:t`
+        -- more information see `vim.fn.expand` or `expand`
+        -- ## only valid after set `show_file = true`
+        file_formatter = "",
+        click_support = true,
+    },
+    outline = {
+        win_position = "right",
+        win_with = "",
+        win_width = 60,
+        show_detail = true,
+        auto_preview = true,
+        auto_refresh = true,
+        auto_close = true,
+        custom_sort = nil,
+        keys = {
+            jump = "o",
+            expand_collapse = "u",
+            quit = "q",
+        },
+    },
 })
 
 
@@ -58,9 +73,7 @@ keymap("n", "<leader>cd", "<cmd>Lspsaga show_cursor_diagnostics<CR>", { silent =
 keymap("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>", { silent = true })
 keymap("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", { silent = true })
 
-
--- Outline
-keymap("n", "<leader>o", "<cmd>LSoutlineToggle<CR>", { silent = true })
+keymap("n", "<leader>o", "<cmd>Lspsaga outline<cr>", { silent = true })
 
 -- Hover Doc
 keymap("n", "K", "<cmd>Lspsaga hover_doc<CR>", { silent = true })
