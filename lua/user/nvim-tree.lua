c564b6b955d71c2fa4af024373f22a0a1c284b89
asyrawih <asyrawih@gmail.com>
2023-04-13 22:18:44 +0700

    update depercated table

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -3,8 +3,6 @@ require 'nvim-tree'.setup {
   disable_netrw       = true,
   hijack_netrw        = true,
   hijack_cursor       = true,
-  open_on_setup       = false,
-  ignore_ft_on_setup  = {},
   open_on_tab         = false,
   update_cwd          = true,
   diagnostics         = {
--- lua/user/sqls.lua
+++ lua/user/sqls.lua
@@ -1,4 +1,4 @@
-require('lspconfig').sqls.setup {
+require('lspconfig').sqlls.setup {
   on_attach = function(client, bufnr)
     require('sqls').on_attach(client, bufnr)
     client.server_capabilities.documentFormattingProvider = false
